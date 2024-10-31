882c3b0a88b02739a14519e38ebb43ac177d9e74
Toby <toby.hatrick@gmail.com>
2024-10-31 19:37:33 +0100

    jobbat på Program och gjort om den till en consolapp

--- Program.cs
+++ Program.cs
@@ -1,9 +1,84 @@
-class Program
+﻿class Program
 {
     static void Main() 
     {
+        while(true)
+        {
+            Console.Clear();
+            Console.WriteLine("Välkommen till våran Bilfirma!"); 
+            Console.WriteLine("Välj din roll:"); 
+            Console.WriteLine("1. Köpare");
+            Console.WriteLine("2. Säljare"); 
+            Console.WriteLine("3. Personal"); 
+            Console.WriteLine("4. Avsluta"); 
+
+            var choice = Console.ReadLine(); 
+
+            switch (choice) 
+            {
+                case "1":
+                    KöpareMeny(); 
+                    break;
+                case "2":
+                    SäljareMeny(); 
+                    break; 
+                case "3": 
+                    PersonalMeny(); 
+                    break; 
+                case "4": 
+                    Console.WriteLine("Avslutar programmet..."); 
+                    return; 
+                // default: Console.WriteLine("Ogiltigt val. Försök igen."); break; 
+            }
+        } 
+    }
+
+    
+    
+    
+
+
+    static void KöpareMeny() 
+    {
+        Console.Clear(); 
+        Console.WriteLine("Köpare Meny"); 
+        Console.WriteLine("1. Visa lagerstatus"); 
+        Console.WriteLine("2. Gå tilbaka till huvudmenyn"); 
         
+        var choice = Console.ReadLine(); 
+        switch (choice)
+        {
+            case "1":
+                VisaLagerstatus(); 
+                break; 
+            case "2": 
+                return; 
+        }
+    }
 
 
+
+    static void SäljareMeny()
+    {
+        Console.Clear(); 
+        Console.WriteLine("Säljare Meny"); 
+        Console.WriteLine("1. Visa lagerstatus"); 
+        Console.WriteLine("2. Lägg til bil till lager"); 
+        Console.WriteLine("3. Gå tillbaka till huvudmenyn"); 
+    }
+
+    static void PersonalMeny() 
+    {
+
+    }
+
+    static void VisaLagerstatus()
+    {
+        Console.Clear(); 
+        Console.WriteLine("Lagerstatus:"); 
+        foreach (var bil in lager)
+        {
+            // bil.Visainfo(); 
+        }
     }
 }
--- Todo.cs
+++ Todo.cs
@@ -1,31 +1,3 @@
 System.Console.WriteLine("hello world!!!!!!!! help pls");
 
 
-
-/*Bilfirma Software
-Projekt: Webbshop för lyxbilar / databas bilar 
-
-3 val logga in 
-- Köpare - Säljare - Personal 
-
-Köpare 
-Visa lagerstatus ( Se lista över alla bilar )
-Tillgänglig, såld, reserverad
-Bilar ska ha egenskaper: årsmodell, miltal, växellåda, märke osv
-
-Säljare 
-Visa lagerstatus ( Se lista över alla bilar (årsmodell, miltal, ) 
-Sälja sin bil ( knappa in egenskaper, inget pris ) 
-
-Personal 
-Visa lagerstatus ( Se lista över alla bilar ) 
-Uppdatera lager ( ändra listan, sätt pris på nya bilar ) 
-Visa försäljningshistorik (logga över tidigare försäljningar)
-Max antal bilar i lager och varningar för lågt lager
-Visa personalstatus (tillgänglighet av personal)
-
-
-
-Framtidsplaner
-(Hantera kundbokningar (bokning av provkörningar eller möten)
-*/

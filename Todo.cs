603694d5def8ab1d264783a219f0be9a03ae2de5
Toby <toby.hatrick@gmail.com>
2024-11-06 17:48:25 +0100

    Uppdaterat Todo.cs

--- Todo.cs
+++ Todo.cs
@@ -1,12 +1,4 @@
-System.Console.WriteLine("hello world!!!!!!!! help pls");
-
-
-
-/*Bilfirma Software
-Projekt: Webbshop för lyxbilar / databas bilar 
-
-3 val logga in 
-- Köpare - Säljare - Personal 
+/*
 
 Köpare 
 Visa lagerstatus ( Se lista över alla bilar )
@@ -14,7 +6,7 @@ Tillgänglig, såld, reserverad
 Bilar ska ha egenskaper: årsmodell, miltal, växellåda, märke osv
 
 Säljare 
-Visa lagerstatus ( Se lista över alla bilar (årsmodell, miltal, ) 
+Visa lagerstatus ( Se lista över alla bilar (årsmodell, miltal, modell) 
 Sälja sin bil ( knappa in egenskaper, inget pris ) 
 
 Personal 
@@ -24,8 +16,10 @@ Visa försäljningshistorik (logga över tidigare försäljningar)
 Max antal bilar i lager och varningar för lågt lager
 Visa personalstatus (tillgänglighet av personal)
 
+KLASSER: 
+Program, Bil, Användare, Köpare, Säljare, Personal (polymorphism)
 
-
-Framtidsplaner
+Framtidsplaner 
 (Hantera kundbokningar (bokning av provkörningar eller möten)
+
 */

d94fd3de08337d76ae7fdd9e0d49755cc838a651
Toby <toby.hatrick@gmail.com>
2024-11-06 17:37:13 +0100

    Uppdaterat Todo.cs

--- Todo.cs
+++ Todo.cs
@@ -1,3 +1,21 @@
-System.Console.WriteLine("hello world!!!!!!!! help pls");
+/*
 
+Köpare 
+Visa lagerstatus ( Se lista över alla bilar )
+Tillgänglig, såld, reserverad
+Bilar ska ha egenskaper: årsmodell, miltal, växellåda, märke osv
 
+Säljare 
+Visa lagerstatus ( Se lista över alla bilar (årsmodell, miltal, modell) 
+Sälja sin bil ( knappa in egenskaper, inget pris ) 
+
+Personal 
+Visa lagerstatus ( Se lista över alla bilar ) 
+Uppdatera lager ( ändra listan, sätt pris på nya bilar ) 
+Visa försäljningshistorik (logga över tidigare försäljningar)
+Max antal bilar i lager och varningar för lågt lager
+Visa personalstatus (tillgänglighet av personal)
+
+KLASSER: 
+Program, Bil, Användare, Köpare, Säljare, Personal (polymorphism)
+*/

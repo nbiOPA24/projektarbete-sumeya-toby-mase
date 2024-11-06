f6daaf30e5ba5c0c1bad53d5880536a878ae4d02
Mase S <masesafi@gmail.com>
2024-11-06 05:27:18 +0100

    blev problem med git merge löstes nu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,125 @@
-Console.WriteLine("HELLO WORLD!"); 
+﻿using System.Runtime.CompilerServices;
 
+namespace Bilfirma;
+
+class Program
+{
+    static List<Car> lager = new List<Car>
+        {
+           
+            // Lista över bilar som finns nu i lagret
+            
+        };
+    static int maxAntalBilar = 20;
+    static void Main() 
+    {
+         
+
+        while(true)
+        {
+            Console.Clear();
+            Console.WriteLine("Välkommen till våran Bilfirma!"); 
+            Console.WriteLine("Välj din roll:"); 
+            Console.WriteLine("1. Köpare");
+            Console.WriteLine("2. Säljare"); 
+            Console.WriteLine("3. Personal"); 
+            Console.WriteLine("4. Avsluta"); 
+
+            var choice = Console.ReadLine(); 
+
+            switch (choice) 
+            {
+                case "1":
+                    KöpareMeny(); 
+                    break;
+                case "2":
+                    SäljareMeny(); 
+                    break; 
+                case "3": 
+                    PersonalMeny(); 
+                    break; 
+                case "4": 
+                    Console.WriteLine("Avslutar programmet..."); 
+                    return; 
+                default: 
+                    Console.WriteLine("Ogiltigt val. Försök igen."); 
+                    break; 
+            }
+        } 
+    }
+
+    
+    
+    
+
+
+    static void KöpareMeny() 
+    {
+        Console.Clear(); 
+        Console.WriteLine("Köpare Meny"); 
+        Console.WriteLine("1. Visa lagerstatus"); 
+        Console.WriteLine("2. Gå tilbaka till huvudmenyn"); 
+        
+        var choice = Console.ReadLine(); 
+        switch (choice)
+        {
+            case "1":
+                VisaLagerstatus(); 
+                break; 
+            case "2": 
+                return; 
+        }
+    }
+
+
+
+    static void SäljareMeny()
+    {
+        Console.Clear(); 
+        Console.WriteLine("Säljare Meny"); 
+        Console.WriteLine("1. Visa lagerstatus"); 
+        Console.WriteLine("2. Lägg til bil till lager"); 
+        Console.WriteLine("3. Gå tillbaka till huvudmenyn"); 
+
+        var choice = Console.ReadLine(); 
+        switch (choice)
+        {
+            case "1":
+                VisaLagerstatus(); 
+                break; 
+            
+        }
+    }
+
+    static void PersonalMeny() 
+    {
+        Console.Clear(); 
+        Console.WriteLine("Personal Meny:"); 
+        Console.WriteLine("Säljare Meny"); 
+        Console.WriteLine("1. Visa lagerstatus"); 
+        Console.WriteLine("2. Uppdatera lager"); 
+        Console.WriteLine("3. Visa försäljningshistorik"); 
+        Console.WriteLine("4. Gå tilbaka till huvudmenyn");
+
+
+        var choice = Console.ReadLine(); 
+        switch (choice)
+        {
+            case "1":
+                VisaLagerstatus(); 
+                break; 
+                
+        }
+
+    }
+
+    static void VisaLagerstatus()
+    {
+        Console.Clear(); 
+        Console.WriteLine("Lagerstatus:"); 
+        foreach (var bil in lager)
+        {
+            // bil.Visainfo(); 
+        }
+    }
+}

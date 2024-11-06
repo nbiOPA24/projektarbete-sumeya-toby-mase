9048124530aada2c8bce14934e9db5dbfc0b9fa8
Toby <toby.hatrick@gmail.com>
2024-11-06 18:01:13 +0100

    Kommentar

--- Användare.cs
+++ Användare.cs
@@ -1,4 +1,4 @@
-public class Användare 
+public class Användare  // Basklass för att hantera grundläggande som namn och roll för varje användartyp
 {
     public string Namn { get; set; }
     public string Roll { get; set; } // "Köpare, "Säljare, "Personal"
